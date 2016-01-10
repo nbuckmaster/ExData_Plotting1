287101800cec036396b2a14818ee575e5afc4d23
Nick Buckmaster <nbuckmaster@gmail.com>
2016-01-10 16:28:28 +1100

    publishing plot1.png and plot1.R

---
+++ plot1.R
@@ -0,0 +1,28 @@
+# this script is in two halves
+#       the first half downloads the data into memory
+#       the second half creates the plot
+
+#       first half - data download
+
+temp <- tempfile()
+
+# DOWNLOADING ....
+# PLEASE WAIT :-)
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+# READING INTO MEMORY....
+data <- read.table(unz(temp, "household_power_consumption.txt"), sep=";", header=TRUE)
+unlink(temp)
+rm(temp)
+
+#       second half - produce the plot from the downloaded data
+
+# convert to date format to perform subsetting
+data$Date <- as.Date(data$Date, format ="%d/%m/%Y")
+# subset the data so we are only looking at the two dates in question
+outdata<-data[data$Date>="2007-02-01" & data$Date<="2007-02-02",]
+# print the plot to the screen device
+hist(as.numeric(outdata$Global_active_power))
+# copy the plot to the file device
+dev.copy(png, "plot1.png")
+# close the file device
+dev.off()
